d58a6d4f70f9935539b77e6d986524efbf839071
fredriick <wayne.fred45@gmail.com>
2024-06-02 23:44:00 +0100

    Deleted typo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,3 @@
-// src/screens/HomeScreen.js
-
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 import BiofeedbackService from '../services/BiofeedbackService';
